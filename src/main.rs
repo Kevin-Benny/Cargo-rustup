c44013c9ba2daf719773e7ed1e23d331d1a05fdf
Kevin-Benny <kevinbenny98@gmail.com>
2023-11-15 20:55:22 +0530

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use std::io;
 fn main() {
     println!("Hello, world!");
     println!("you are at branch Checkpoint!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+
+    
+    //panic!("Problem reading:{:?}",Err::<T, ()>(()));
+    //code to print error from reading line as well as the result
+    //io::stdin().read_line(&mut guess).expect(Err(()));
+    
+    
+
+    println!("You guessed: {guess}");
 }
